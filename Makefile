667ee14cdce40769eaeb370aade8c57c1d25e937
Morgan Arnold <morgan.arnold@protonmail.com>
2022-04-08 10:53:37 -0700

    Add code to load FAT clusters from disk.
    This commit adds a short subroutine to load a single FAT cluster from the disk. In the future, another subroutine will be added which searches the FAT for the next cluster, so traversing cluster chains will be possible. This also removes some printing subroutines to save space. They may be replaced if there is room once the bootloader is working.

--- Makefile
+++ Makefile
@@ -1,14 +1,19 @@
-all: bootloader disk
+all: bootloader filesystem disk
 
 bootloader:
 	nasm -o target/bootloader src/stage1.s
-disk: bootloader
-	dd if=/dev/zero of=target/disk.iso count=2049 conv=notrunc
-	parted -s target/disk.iso mktable msdos
-	parted -s target/disk.iso mkpart primary fat32 0 100%
-	parted -s target/disk.iso set 1 boot on
+filesystem:
+	mformat -FC -i target/filesystem.img -T 66581
+	mmd -i target/filesystem.img ::/boot
+	nasm -o target/boot.bin src/stage2.s
+	mcopy -i target/filesystem.img target/boot.bin ::/boot/boot.bin
+disk: bootloader filesystem
+	dd if=/dev/zero of=target/disk.img count=66582 conv=notrunc
+	parted -s target/disk.img mktable msdos
+	parted -s target/disk.img mkpart primary fat32 0 100%
+	parted -s target/disk.img set 1 boot on
 	dd if=target/bootloader of=target/disk.iso bs=1 count=446 conv=notrunc
 	dd if=target/bootloader of=target/disk.iso bs=1 count=2 skip=510 seek=510 conv=notrunc
-	mkfs.fat -F32 --offset=1 target/disk.iso
+	dd if=target/filesystem.img of=target/disk.iso seek=1 conv=notrunc
 qemu: disk
-	qemu-system-x86_64 target/disk.iso
+	qemu-system-x86_64 target/disk.img
--- src/stage1.s
+++ src/stage1.s
@@ -17,15 +17,10 @@ stage1:
     jmp 0x0:.enforce_csip
 .enforce_csip:
 
-    ; Print welcome message.
-    mov si, boot_message
-    call print
-
     ; Error if the boot disk does not support INT 13h extensions.
     mov ah, 0x41
     mov bx, 0x55AA
     int 0x13
-    jc no_extensions_error
 
     ; Identify the bootable partition.
     mov bx, partition_table
@@ -36,7 +31,6 @@ stage1:
     je .bootable_partition_found
     add bx, 0x10
     loop .loop
-    jmp stage2_load_error
 .bootable_partition_found:
 
     ; Load bootable partition Boot Record.
@@ -46,56 +40,45 @@ stage1:
     mov ah, 0x42
     mov si, data_address_packet
     int 0x13
-    jc stage2_load_error
 
-    ; Locate and load C:/boot/bootloader.bin.
-    ; TODO
-
-    ; Jump to stage 2.
-    ; TODO
-
-    jmp halt
-
-no_extensions_error:
-    mov si, no_extensions_error_message
-    call print
-    jmp halt
-
-stage2_load_error:
-    mov si, stage2_load_error_message
-    call print
-    jmp halt
+    ; Load first cluster of / into memory.
+    mov eax, [boot_record.root_cluster]
+    mov si, file
+    call load_cluster
 
 halt:
     hlt
     jmp halt
 
-; Print a string.
+; load a cluster into memory
 ; IN
-;   si: pointer to null-terminated string to be printed
-; CLOBBER
-;   ax, si
-print:
-    pushf
-    cld
-.loop:
-    lodsb
-    cmp al, 0
-    je .return
-    call print_char
-    jmp .loop
-.return:
-    popf
-    ret
-
-; Print a character.
-; IN
-;   al: character to be printed
-print_char:
+;   eax: cluster address
+;   si:  address at which to load cluster
+load_cluster:
     pusha
+
     xor bx, bx
-    mov ah, 0x0E
-    int 0x10
+    mov bl, [boot_record.cluster_size]
+    mov [data_address_packet.count], bx
+
+    mov [data_address_packet.offset], si
+
+    mov ebx, [boot_record.hidden_sector_count]
+    mov [data_address_packet.address], ebx
+    mov bx, [boot_record.reserved_sector_count]
+    add [data_address_packet.address], bx
+    mov ebx, [boot_record.fat_size]
+    mov cl, [boot_record.fat_count]
+.loop:
+    add [data_address_packet.address], ebx
+    loop .loop
+
+    add [data_address_packet.address], eax
+
+    mov ah, 0x42
+    mov si, data_address_packet
+    int 0x13
+
     popa
     ret
 
@@ -111,13 +94,51 @@ data_address_packet:
 .address:
     dq 0
 
-boot_message: db "Bootloader - Stage One", 0
-no_extensions_error_message: db "Error: Disk does not support INT 13h extensions.", 0
-stage2_load_error_message: db "Error: Failed to load Stage Two.", 0
+boot_dir_name: db "boot       "
+boot_bin_name: db "boot    bin"
 
 times 446-($-$$) db 0
 partition_table: times 4 * 16 db 0
 db 0x55
 db 0xAA
 
+SECTION .bss
 boot_record:
+    resb 3
+    resb 8
+    resb 2
+.cluster_size:
+    resb 1
+.reserved_sector_count:
+    resb 2
+.fat_count:
+    resb 1
+    resb 2
+    resb 2
+    resb 1
+    resb 2
+    resb 2
+    resb 2
+.hidden_sector_count:
+    resb 4
+    resb 4
+.fat_size:
+    resb 4
+    resb 2
+    resb 2
+.root_cluster:
+    resb 4
+    resb 2
+    resb 2
+    resb 12
+    resb 1
+    resb 1
+    resb 1
+    resb 4
+    resb 11
+    resb 8
+    resb 420
+    resb 2
+fat:
+    resb 512
+file:
