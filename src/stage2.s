6a787305671f03725a2f8345b2d6597eb2421ef0
Morgan Arnold <morgan.arnold@protonmail.com>
2022-04-11 21:15:06 -0700

    Implement A20 line checking.
    This commit introduces the check_a20 subroutine, which checks whether or not the A20 line is enabled. If the A20 line is not enabled, we will have to try a variety of methods of enabling it.

---
+++ src/stage2.s
@@ -0,0 +1,61 @@
+ORG 0x8200
+SECTION .text
+USE16
+stage2:
+    ; Check if the A20 line is already enabled.
+    call check_a20
+    cmp ax, 1
+    je .a20_enabled
+
+    ; Attempt to enable the A20 line.
+    ; TODO
+
+.a20_enabled:
+
+halt:
+    hlt
+    jmp halt
+
+; check if the A20 line is enabled
+; OUT
+;   ax: 0 if the A20 line is disabled, 1 if it is enabled
+check_a20:
+    push ds
+    push di
+    push ss
+    push si
+
+    mov ax, 0x0000
+    mov ds, ax
+    mov di, 0x7DFE
+    mov ax, 0xFFFF
+    mov ss, ax
+    mov si, 0x7E0E
+
+    ; The boot signature (0x55AA) is located at 0x0000:0x7DFE. If 0xFFFF:0x7E0E
+    ; contains a different value, the A20 line must be enabled.
+    cmp word [ss:si], 0x55AA
+    jne .a20_enabled
+
+    ; It is still possible that the A20 line is enabled and 0x0000:0x7DFE just
+    ; happens to contain 0x55AA. Just in case this is the case, we overwrite
+    ; the value at 0x0000:0x7DFE and check if 0x0000:0x7DFE still contains the
+    ; boot signature. If it does, the A20 line must still be enabled.
+    mov word [ss:si], 0
+    cmp word [ds:di], 0x55AA
+    mov word [ss:si], 0x55AA
+    je .a20_enabled
+
+    mov ax, 0
+    jmp .return
+
+.a20_enabled:
+    mov ax, 1
+
+.return:
+    pop si
+    pop ss
+    pop di
+    pop ds
+
+    ret
